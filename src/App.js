e39156fe3b6a2c8dc802d28dcee56716ab928dd1
puneetprht <puneet_prht@yahoo.co.in>
2021-11-07 18:38:15 +0530

    Add video element

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import ReactDOM from "react-dom";
+import video from './videoplayback.mp4';
 import './App.css';
 
 function App() {
+  // useEffect(() => {
+  //   playVideo();
+  // }, [vid])
+   
+  // // var vid = ReactDOM.findDOMNode(document.getElementById("myVideo"));
+  // // console.log(vid);
+  // // function playVideo() {
+  // //   vid.play();
+  // // }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,8 +27,13 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Create new page
+        </a> */}
+        <div className="videoContainer">
+          <video width="300" height="200" autoplay controls>
+            <source src={video} type="video/mp4"/>
+          </video>    
+        </div>
       </header>
     </div>
   );
